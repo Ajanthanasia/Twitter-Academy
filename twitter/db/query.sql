0f8b196f638d3e3cd16811809fe358e7bdd78cf2
Ajanthan <ajanthanasia@gmail.com>
2024-03-14 22:17:09 +0530

    db change

--- twitter/db/query.sql
+++ twitter/db/query.sql
@@ -13,7 +13,6 @@ CREATE TABLE `convo` (
     PRIMARY KEY (`id`)
 );
 
-
 INSERT INTO
     `convo`
 VALUES
@@ -29,7 +28,7 @@ INSERT INTO
     `convo_users`
 VALUES
     (1, 1, '2024-02-21 09:45:54'),
-(1, 2, '2024-02-21 09:45:56');
+    (1, 2, '2024-02-21 09:45:56');
 
 CREATE TABLE `follow` (
     `id_user` int NOT NULL,
@@ -41,7 +40,6 @@ INSERT INTO
     `follow`
 VALUES
     (1, 2, '2024-02-21 09:41:13');
-
 
 CREATE TABLE `hashtag_list` (
     `hashtag` varchar(255) DEFAULT NULL,
@@ -59,8 +57,7 @@ INSERT INTO
     `likes`
 VALUES
     (2, 1, '2024-02-21 09:44:56'),
-(2, 3, '2024-02-21 09:45:01');
-
+    (2, 3, '2024-02-21 09:45:01');
 
 CREATE TABLE `messages` (
     `id` int NOT NULL AUTO_INCREMENT,
@@ -90,63 +87,17 @@ CREATE TABLE `reactions` (
     `reaction` varchar(20) DEFAULT NULL
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `reactions`
---
-LOCK TABLES `reactions` WRITE;
-
-/*!40000 ALTER TABLE `reactions` DISABLE KEYS */
-;
-
-/*!40000 ALTER TABLE `reactions` ENABLE KEYS */
-;
-
-UNLOCK TABLES;
-
---
--- Table structure for table `retweet`
---
-DROP TABLE IF EXISTS `retweet`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-
-/*!50503 SET character_set_client = utf8mb4 */
-;
-
 CREATE TABLE `retweet` (
     `id_user` int NOT NULL,
     `id_tweet` int NOT NULL,
     `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `retweet`
---
-LOCK TABLES `retweet` WRITE;
-
-/*!40000 ALTER TABLE `retweet` DISABLE KEYS */
-;
-
 INSERT INTO
     `retweet`
 VALUES
     (1, 2, '2024-02-21 09:44:00');
 
-/*!40000 ALTER TABLE `retweet` ENABLE KEYS */
-;
-
-UNLOCK TABLES;
-
---
--- Table structure for table `signaled`
---
 CREATE TABLE `signaled` (
     `id_user` int NOT NULL,
     `id_signaled` int NOT NULL,
@@ -154,22 +105,6 @@ CREATE TABLE `signaled` (
     `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `signaled`
---
---
--- Table structure for table `subscriptions`
---
-DROP TABLE IF EXISTS `subscriptions`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-
-/*!50503 SET character_set_client = utf8mb4 */
-;
 
 CREATE TABLE `subscriptions` (
     `id` int NOT NULL AUTO_INCREMENT,
@@ -179,32 +114,6 @@ CREATE TABLE `subscriptions` (
     PRIMARY KEY (`id`)
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `subscriptions`
---
-LOCK TABLES `subscriptions` WRITE;
-
-/*!40000 ALTER TABLE `subscriptions` DISABLE KEYS */
-;
-
-/*!40000 ALTER TABLE `subscriptions` ENABLE KEYS */
-;
-
-UNLOCK TABLES;
-
---
--- Table structure for table `token`
---
-DROP TABLE IF EXISTS `token`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-
-/*!50503 SET character_set_client = utf8mb4 */
-;
 
 CREATE TABLE `token` (
     `id` int NOT NULL AUTO_INCREMENT,
@@ -213,33 +122,6 @@ CREATE TABLE `token` (
     `now` DATETIME NOT NULL,
     PRIMARY KEY (`id`)
 );
-
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `token`
---
-LOCK TABLES `token` WRITE;
-
-/*!40000 ALTER TABLE `token` DISABLE KEYS */
-;
-
-/*!40000 ALTER TABLE `token` ENABLE KEYS */
-;
-
-UNLOCK TABLES;
-
---
--- Table structure for table `tweet`
---
-DROP TABLE IF EXISTS `tweet`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-
-/*!50503 SET character_set_client = utf8mb4 */
-;
 
 CREATE TABLE `tweet` (
     `id` int NOT NULL AUTO_INCREMENT,
@@ -251,16 +133,6 @@ CREATE TABLE `tweet` (
     PRIMARY KEY (`id`)
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `tweet`
---
-LOCK TABLES `tweet` WRITE;
-
-/*!40000 ALTER TABLE `tweet` DISABLE KEYS */
-;
 
 INSERT INTO
     `tweet`
@@ -273,24 +145,16 @@ VALUES
         'SALUT VOICI UN TWEET DE SYPHAX',
         NULL
     ),
-(2, 2, 1, '2024-02-21 10:42:53', 'bon tweet ça', NULL),
-(3, 1, 2, '2024-02-21 10:43:17', 'merci', NULL);
+    (
+        2,
+        2,
+        1,
+        '2024-02-21 10:42:53',
+        'bon tweet ça',
+        NULL
+    ),
+    (3, 1, 2, '2024-02-21 10:43:17', 'merci', NULL);
 
-/*!40000 ALTER TABLE `tweet` ENABLE KEYS */
-;
-
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-DROP TABLE IF EXISTS `user`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-
-/*!50503 SET character_set_client = utf8mb4 */
-;
 
 CREATE TABLE `user` (
     `id` int NOT NULL AUTO_INCREMENT,
@@ -311,16 +175,6 @@ CREATE TABLE `user` (
     UNIQUE KEY `mail` (`mail`)
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */
-;
-
---
--- Dumping data for table `user`
---
-LOCK TABLES `user` WRITE;
-
-/*!40000 ALTER TABLE `user` DISABLE KEYS */
-;
 
 INSERT INTO
     `user`
@@ -340,7 +194,7 @@ VALUES
         'Paris',
         NULL
     ),
-(
+    (
         2,
         'Romain',
         '@ronron',
@@ -355,37 +209,3 @@ VALUES
         'Paris',
         NULL
     );
-
-/*!40000 ALTER TABLE `user` ENABLE KEYS */
-;
-
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'twitterV'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
-;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
-;
-
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
-;
-
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
-;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
-;
-
--- Dump completed on 2024-02-21 15:40:06
