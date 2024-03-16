623a495e90a2e0254931aa632fa64161b74bd26f
Ajanthan <ajanthanasia@gmail.com>
2024-03-16 21:53:23 +0530

    date updated

--- twitter/routes/postStore.php
+++ twitter/routes/postStore.php
@@ -2,7 +2,7 @@
 include("databaseConnection.php");
 $tweet = $_POST['tweet'];
 $userId = $_POST['userId'];
-$date = date('d-m-y h:i:s');
+$date = date('y-m-d h:i:s');
 $query = "INSERT INTO tweet VALUES ('','$userId','','$date','$tweet','')";
 $queryLoad = mysqli_query($conn, $query);
 echo true;
