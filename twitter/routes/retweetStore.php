af0d7fad849f33358c9535d257ab9b9acdf46d3e
Ajanthan <ajanthanasia@gmail.com>
2024-03-17 00:01:14 +0530

    time update

--- twitter/routes/retweetStore.php
+++ twitter/routes/retweetStore.php
@@ -2,7 +2,7 @@
 include("databaseConnection.php");
 $userId = $_POST['userId'];
 $tweetId = $_POST['tweetId'];
-$date = date('y-m-d h:i:s');
+$date = date('y-m-d H:i:s');
 $query = "INSERT INTO retweet VALUES ('$userId','$tweetId','$date')";
 $load = mysqli_query($conn, $query);
 echo 'Retweeted successfully!';
