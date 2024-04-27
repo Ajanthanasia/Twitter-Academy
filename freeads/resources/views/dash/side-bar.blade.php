589f8f6b1fe9e63b1f4b6c60f99bddddb96d73e0
Ajanthan <ajanthanasia@gmail.com>
2024-04-28 00:20:59 +0530

    done

--- freeads/resources/views/dash/side-bar.blade.php
+++ freeads/resources/views/dash/side-bar.blade.php
@@ -21,17 +21,16 @@
  <a href="#">
     <button>Search</button>
  </a> -->
- 
- 
-    <a href="{{ route('user.edit') }}"><button>Edit Profile</button></a>
-    <hr>
-    <a href="{{ route('logout') }}"><button>Logout</button></a>
-    <hr>
-    <a href="{{ route('user.destroy', ['id' => Auth::id()]) }}"><button>Delete Account</button></a>
-    <hr>
-    <a href="{{ route('ad.add.create') }}"><button>New Ad</button></a>
-    <hr>
-    <a href="{{ route('ad.all.index') }}"><button>My Ads</button></a>
-    <hr>
-    <a href="#"><button>Search</button></a>
 
+
+ <a href="{{ route('user.edit') }}"><button>Edit Profile</button></a>
+ <hr>
+ <a href="{{ route('logout') }}"><button>Logout</button></a>
+ <hr>
+ <a href="{{ route('user.destroy', ['id' => Auth::id()]) }}"><button>Delete Account</button></a>
+ <hr>
+ <a href="{{ route('ad.add.create') }}"><button>New Ad</button></a>
+ <hr>
+ <a href="{{ route('ad.all.index') }}"><button>My Ads</button></a>
+ <hr>
+ <a href="{{ route('search.index') }}"><button>Search</button></a>
